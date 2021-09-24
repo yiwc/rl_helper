aa3fe485db93e9a0344507d278ede01e9c6e7115
cyw <670070172@qq.com>
2021-09-25 00:22:36 +0800

    support fps with vec env gym

--- rl_helper/fps.py
+++ rl_helper/fps.py
@@ -1,14 +1,23 @@
 import time
+import numpy as np
+from gym.vector.async_vector_env import AsyncVectorEnv
+from gym.vector.sync_vector_env import SyncVectorEnv
+from gym.vector.vector_env import VectorEnv, VectorEnvWrapper
 def fps(env,steps_per_test=100,episode=10):
+    
+    result_multiply=env.num_envs if isinstance(env,AsyncVectorEnv) else 1
     for e in range(episode):
         count=0
         start_t=time.time()
         for i in range(steps_per_test):
             a = env.action_space.sample()  # Sample an action
             obs, reward, done, info = env.step(a)  # Step the environoment with the sampled random action
-            if done:
-                obs = env.reset()  # Reset environment
-            # env.step(env.action_space.sample())
+            if isinstance(done,int):
+                if done:
+                    obs = env.reset()
+            elif isinstance(done,np.ndarray):
+                if done.any():
+                    obs = env.reset()  # Reset environment
         env.reset() 
         dt=time.time()-start_t
-        print("fps = {}".format(steps_per_test/dt))
+        print("fps = {}".format(steps_per_test/dt*result_multiply))
