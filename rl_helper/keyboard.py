cd2c97d86fa2cc56b3c72a956be1a0ef2e6632ee
yw <cywgoog@gmail.com>
2022-01-02 13:31:00 +0800

    v0.4

--- rl_helper/keyboard.py
+++ rl_helper/keyboard.py
@@ -38,29 +38,14 @@ class kb(object):
         self.last_action=None
         return action
 
-class kb2(object):
-    def __init__(self,key2action) -> None:
-        super().__init__()
-        import threading
-        listen=threading.Thread(target=self.listener)
-        listen.start()
-
-    def listener(self):
-        while True:  
-            time.sleep(0.05)
-            try:  
-                print(keyboard.read_key())
-                      
-            except Exception as err:
-                print(err)
-                break   
 
 class kbinteractor(object):
-    def __init__(self,env,kb:kb) -> None:
+    def __init__(self,env,key2action:dict) -> None:
         super().__init__()
+        # self.k=
         self.env=env
-        self.kb=kb
-
+        self.kb=kb(key2action=key2action)
+        self.run()
     def run(self):
         import matplotlib.pyplot as plt
         import numpy as np
@@ -68,35 +53,33 @@ class kbinteractor(object):
         fig, ax = plt.subplots()
 
         self.env.reset()
-        k.start()
+        img=self.env.render("rgb_array")
+        ax.cla()
+        ax.imshow(img)
+        plt.pause(0.1)
+        self.env.reset()
+        self.kb.start()
         while True:
-            obs,rew,dones,info=env.step(k.get_action())
+            obs,rew,dones,info=self.env.step(self.kb.get_action())
             print('rew',rew)
             if dones:
-                env.reset()
-            img=env.render("rgb_array")
+                self.env.reset()
+            img=self.env.render("rgb_array")
             ax.cla()
             ax.imshow(img)
-            ax.set_title("frame {}".format("rgb"))
             plt.pause(0.1)
 
 if __name__=="__main__":
 
     import gym
-    k=kb(
-        {
-            'right':[1,0,0,0],
-            'left':[-1,0,0,0],
-            'up':[0,1,0,0],
-            'down':[0,-1,0,0],
-            "y":[0,0,1,0],
-            "h":[0,0,-1,0],
-            "n":[0,0,0,1],
-            "b":[0,0,0,-1],
-        }
-    )
-    
-    env = gym.make('FetchPickAndPlaceDense-v1')
-    env.reset()
-    interactor=kbinteractor(env=env,kb=k)
-    interactor.run()
+    interactor=kbinteractor(env=gym.make('FetchPickAndPlaceDense-v1'),
+            key2action={
+                    'right':[1,0,0,0],
+                    'left':[-1,0,0,0],
+                    'up':[0,1,0,0],
+                    'down':[0,-1,0,0],
+                    "y":[0,0,1,0],
+                    "h":[0,0,-1,0],
+                    "n":[0,0,0,1],
+                    "b":[0,0,0,-1],
+                })
